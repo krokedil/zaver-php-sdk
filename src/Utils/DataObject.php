8d45e5e74bca165a8a12dd4086028342f9db0843
Ivar Johansson <ivar@webbmaffian.se>
2022-02-03 10:25:04 +0100

    Ensure that any setters are used when using DataObject constructor

--- src/Utils/DataObject.php
+++ src/Utils/DataObject.php
@@ -10,7 +10,16 @@ abstract class DataObject implements JsonSerializable {
 	}
 
 	public function __construct(array $data = []) {
-		$this->data = $data;
+		foreach($data as $key => $value) {
+			$method = 'set' . ucfirst($key);
+
+			if(method_exists($this, $method)) {
+				$this->$method($value);
+			}
+			else {
+				$this->data[$key] = $value;
+			}
+		}
 	}
 
 	public function jsonSerialize() {
