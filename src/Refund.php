d08cd7548b1a049841478d66b6c2f064097a3cd2
Ivar Johansson <ivar@webbmaffian.se>
2022-02-09 11:38:50 +0100

    Added refund callback

--- src/Refund.php
+++ src/Refund.php
@@ -1,9 +1,12 @@
 <?php
 namespace Zaver\SDK;
 use Zaver\SDK\Utils\Base;
+use Zaver\SDK\Utils\Error;
+use Zaver\SDK\Utils\Helper;
 use Zaver\SDK\Object\RefundCreationRequest;
 use Zaver\SDK\Object\RefundResponse;
 use Zaver\SDK\Object\RefundUpdateRequest;
+use Exception;
 
 class Refund extends Base {
 	public function createRefund(RefundCreationRequest $request): RefundResponse {
@@ -28,5 +31,24 @@ class Refund extends Base {
 		$response = $this->client->post(sprintf('/refund/v1/%s/cancel', $refundId), $request);
 
 		return new RefundResponse($response);
+	}
+
+	public function receiveCallback(?string $callbackKey = null): RefundResponse {
+		if(!is_null($callbackKey) && !hash_equals($callbackKey, Helper::getAuthorizationKey())) {
+			throw new Error('Invalid callback key');
+		}
+
+		try {
+			if($_SERVER['HTTP_METHOD'] !== 'POST') {
+				throw new Error('Invalid HTTP method');
+			}
+			
+			$data = json_decode(file_get_contents('php://input'), true, 10, JSON_THROW_ON_ERROR);
+		}
+		catch(Exception $e) {
+			throw new Error('Failed to decode Zaver response', null, $e);
+		}
+
+		return new RefundResponse($data);
 	}
 }
