78763e72d177c7ee4e1ee1b2143c8b8f97f16315
Mattias Forsman <forsman1991@gmail.com>
2023-01-04 13:57:21 +0100

    Fix return type and add method alias

--- src/Object/PaymentMethod.php
+++ src/Object/PaymentMethod.php
@@ -5,11 +5,17 @@ use Zaver\SDK\Utils\DataObject;
 /**
  * PaymentMethod as found in `PaymentMethodsResponse`
  * 
- * @method float          getPaymentMethod()    The Payment Method identifier.
+ * @method string         getPaymentMethod()    The Payment Method identifier.
  * @method string         getTitle()            The title of the payment method for display purposes.
  * @method string         getDescription()      A short text that describes the payment method.
  * @method string         getIconSvgSrc()       A URL to an SVG icon for the payment method.
  */
 class PaymentMethod extends DataObject {
 
+	/**
+	 * Alias for `getPaymentMethod`
+	 */
+	public function getIdentifier(): string {
+		return $this->getPaymentMethod();
+	}
 }
