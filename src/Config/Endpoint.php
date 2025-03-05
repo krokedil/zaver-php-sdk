9ac18296be3e94b6ac9cb8ca55f1c945ca25d91e
MichaelBengtsson <michael@krokedil.se>
2025-03-05 10:34:59 +0100

    Change from .se to .com in endpoint config

--- src/Config/Endpoint.php
+++ src/Config/Endpoint.php
@@ -3,13 +3,13 @@ namespace Zaver\SDK\Config;
 
 class Endpoint {
 	/** API endpoint to production environment */
-	const PRODUCTION = 'https://api.zaver.se';
+	const PRODUCTION = 'https://api.zaver.com';
 
 	/** URL to script in production environment */
 	const PRODUCTION_SCRIPT = 'https://iframe-checkout.zaver.se/loader/v1.js';
 
 	/** API endpoint to test environment */
-	const TEST = 'https://api.test.zaver.se';
+	const TEST = 'https://api.test.zaver.com';
 
 	/** URL to script in test environment */
 	const TEST_SCRIPT = 'https://iframe-checkout.test.zaver.se/loader/v1.js';
